1f64b055ad5ea8671741ad9b83c6c1c6772478ac
makrenka <smokrenko79@gmail.com>
2023-06-08 19:43:27 +0300

    using RTK-query

--- src/store/index.js
+++ src/store/index.js
@@ -1,19 +1,14 @@
 import { configureStore } from '@reduxjs/toolkit';
 
+import { heroesApiSlice } from '../api/heroesApiSlice';
 import filters from './filtersSlice';
-import heroes from './heroesSlice';
-
-// const store = createStore(
-//   combineReducers({ heroesReducer, filtersReducer }),
-//   compose(
-//     applyMiddleware(ReduxThunk),
-//     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
-//   )
-// );
-
-// export default store;
 
 export const store = configureStore({
-  reducer: { heroes, filters },
+  reducer: {
+    [heroesApiSlice.reducerPath]: heroesApiSlice.reducer,
+    filters
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(heroesApiSlice.middleware),
   devTools: process.env.NODE_ENV !== 'production',
 });
